eec4e9cf4485e84ab7c4c75f3a52120024f2eec0
Kimani <elvis.kimani@student.moringaschool.com>
2022-08-18 16:02:31 +0300

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
 def oxford_comma(array)
-
+    length = array.size
+    
+        if length == 1
+            return array.join
+        elsif length == 2
+            lastelem = array.pop
+            array.push(" and ")
+            array.push (lastelem)
+            return array.join
+        else length >=3
+            lastelem = array.pop  
+            array.push("and ")
+            cad = array.join(', ')
+            cad = cad + lastelem
+            return cad
+           
+        end
+  
 end
